c1e1ae0daeb9f9adacee8a0ee4bb8159d5ef09ed
kuas6678 <kuas6678@gmail.com>
2017-01-01 17:57:27 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 /**
  * Module dependencies.
  */
- //---------------------
+//---------------------
 require('./lib/db');
 //--------------------
 var express = require('express');
@@ -14,7 +13,7 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8080); // listening 8080 port
 //設定存放模板的目錄位置
 app.set('views', path.join(__dirname, 'views'));
 //設定所要選用的模板引擎，副檔名為.jade，可使用res.render()引用它
@@ -25,15 +24,15 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 //-----------------------------------------------
-app.configure(function(){
-	app.use(express.cookieParser());
-	
-	app.use(express.cookieSession({
-		key: 'node',
-		secret: 'HelloExpressSESSION'
-	}));
-	//啟用body解析器
-	app.use(express.bodyParser());
+app.configure(function() {
+    app.use(express.cookieParser());
+
+    app.use(express.cookieSession({
+        key: 'node',
+        secret: 'HelloExpressSESSION'
+    }));
+    //啟用body解析器
+    app.use(express.bodyParser());
 });
 //------------------------------------------------
 app.use(app.router); //啟用路由機制
@@ -41,23 +40,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 //------------------------------------------------
-app.get('/mydir/hello', function(req, res){ //QueryString 練習
-	console.log(req.query.name);
-	console.log(req.query.country);
+app.get('/mydir/hello', function(req, res) { //QueryString 練習
+    console.log(req.query.name);
+    console.log(req.query.country);
 });
 
-app.post('/mydir/hello', function(req, res){ //POST方法
-	console.log(req.body.name);
-	console.log(req.body.country);
+app.post('/mydir/hello', function(req, res) { //POST方法
+    console.log(req.body.name);
+    console.log(req.body.country);
 });
 
-app.get('/mydir/hello/:name', function(req, res){ //使用路由參數的方式來得到資料，直接將網址路徑本身變成傳遞參數的容器
-	res.send("name: "+req.params.name);           //exp: 傳遞一個名字[QQ]，/mydir/hello/QQ
-	console.log(req.params.name);             
-	res.end();
+app.get('/mydir/hello/:name', function(req, res) { //使用路由參數的方式來得到資料，直接將網址路徑本身變成傳遞參數的容器
+    res.send("name: " + req.params.name); //exp: 傳遞一個名字[QQ]，/mydir/hello/QQ
+    console.log(req.params.name);
+    res.end();
 });
 
 
@@ -76,6 +75,6 @@ app.get('/apis/delete/:id', user.del_article);
 app.post('/apis/update/:id', user.update);
 app.post('/apis/comment/:id', user.comment);
 //---------------------------------------------------
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
